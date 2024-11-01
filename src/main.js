054e41da33da4586cfa9878fcdf440e4a9d02fa4
dconco <onipededavid208@gmail.com>
2024-11-01 11:04:11 +0100

    Fixed error in default code

--- src/main.js
+++ src/main.js
@@ -1,26 +1,31 @@
-import plugin from '../plugin.json';
+import plugin from '../plugin.json'
+const alert = acode.require('alert')
 
 class AcodePlugin {
+	async init() {
+		const handleOnHide = () => {
+			window.toast('Alert modal closed', 4000)
+		}
 
-  async init() {
+		alert('Title of Alert', 'The alert body message..', handleOnHide)
+	}
 
-  }
-
-  async destroy() {
-
-  }
+	async destroy() {}
 }
 
 if (window.acode) {
-  const acodePlugin = new AcodePlugin();
-  acode.setPluginInit(plugin.id, async (baseUrl, $page, { cacheFileUrl, cacheFile }) => {
-    if (!baseUrl.endsWith('/')) {
-      baseUrl += '/';
-    }
-    acodePlugin.baseUrl = baseUrl;
-    await acodePlugin.init($page, cacheFile, cacheFileUrl);
-  });
-  acode.setPluginUnmount(plugin.id, () => {
-    acodePlugin.destroy();
-  });
+	const acodePlugin = new AcodePlugin()
+	acode.setPluginInit(
+		plugin.id,
+		async (baseUrl, $page, { cacheFileUrl, cacheFile }) => {
+			if (!baseUrl.endsWith('/')) {
+				baseUrl += '/'
+			}
+			acodePlugin.baseUrl = baseUrl
+			await acodePlugin.init($page, cacheFile, cacheFileUrl)
+		}
+	)
+	acode.setPluginUnmount(plugin.id, () => {
+		acodePlugin.destroy()
+	})
 }
